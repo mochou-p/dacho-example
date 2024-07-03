e9eb3bbb4dc474efb5cdec330b1b5b3493e4de96
mochou-p <vrecusko20@gmail.com>
2024-07-03 13:10:41 +0200

    2D/3D Camera

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,14 @@
 use dacho::prelude::*;
 
 fn main() {
-    World::demo()
+    World::new()
+        .add(&[//                                         zoom ar        near   far
+            Camera::new(V3::new(0.0, 0.0, 10.0), Camera2D(1.0, 16.0/9.0, 0.001, 10000.0))
+            // Camera::default()
+                .build(),
+            Sphere::default()
+                .build()
+        ])
         .run();
 }
 
