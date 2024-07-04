fabeb2bb81f1ca21ffc2491a8dc8d60edb6cd06e
mochou-p <vrecusko20@gmail.com>
2024-07-04 19:04:34 +0200

    cleanup Object implementation and usage

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,18 @@ use dacho::prelude::*;
 
 fn main() {
     World::new()
-        .add(&[//                                         zoom ar        near   far
-            Camera::new(V3::new(0.0, 0.0, 10.0), Camera2D(1.0, 16.0/9.0, 0.001, 10000.0))
-            // Camera::default()
-                .build(),
-            Sphere::default()
-                .build()
+        .add(&[
+            Camera(
+                Perspective {
+                    position:     V3::Z * 7.0,
+                    fov:          45.0,
+                    aspect_ratio: 16.0/9.0,
+                    near:         0.0001,
+                    far:          10000.0
+                }
+            ),
+            Shape(Cube   { position: V3::ZERO,     size:   V3::new(2.0, 0.1, 2.0) }),
+            Shape(Sphere { position: V3::Y * 0.55, radius: 0.5                    })
         ])
         .run();
 }
