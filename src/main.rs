9a3d491ff70e3590457157fdc7d01937372cb53d
mochou-p <66534926+mochou-p@users.noreply.github.com>
2024-07-14 01:23:10 +0200

    simple Component insertion

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,35 @@ use dacho::prelude::*;
 fn main() {
     let mut world = World::new();
 
-    world.add("dog");
-    world.add("cat");
+    world.spawn("dog");
+    world.spawn("cat");
 
-    let player_id = world.add("player");
-    let player    = world.get(player_id).expect("player == None");
-    println!("this should be player -> {}\n", player.name);
+    let (_, player) = world.spawn("player");
 
-    world.add("car");
+    player.add(ComponentA { name: "A".to_string() });
+    player.add(ComponentB);
+
+    let (car_id, _) = world.spawn("car");
+    println!("car = {}\n", world.get(car_id).expect("car is None").name);
 
     world.debug();
+}
+
+struct ComponentA {
+    pub name: String
+}
+
+impl Component for ComponentA {
+    fn name(&self) -> &str {
+        &self.name
+    }
+}
+
+struct ComponentB;
+
+impl Component for ComponentB {
+    fn name(&self) -> &str {
+        "B"
+    }
 }
 
