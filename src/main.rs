c5507f81a2511d651c067ed087e16918772ae13c
mochou-p <66534926+mochou-p@users.noreply.github.com>
2024-07-14 00:46:48 +0200

    simple Entity creation and retrieval

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,18 @@
 
 use dacho::prelude::*;
 
-use {
-    InnerCamera::Perspective,
-    InnerShape2D::{Quad, Circle},
-    InnerShape3D::{Cube, Sphere}
-};
-
-use Object::{Camera, Shape2D, Shape3D};
-
 fn main() {
-    World::new()
-        .add(&[
-            Camera(
-                Perspective {
-                    position:     V3::new(2.5, 1.0, 7.5),
-                    fov:          45.0,
-                    aspect_ratio: 16.0/9.0,
-                    near:         0.0001,
-                    far:          10000.0
-                }
-            ),
+    let mut world = World::new();
 
-            Shape2D(Quad   { position: V3::X * -2, size:   V2::ONE        }),
-            Shape2D(Circle { position: V3::X *  2, radius: 0.5, points: 3 }),
-            Shape2D(Circle { position: V3::X *  3, radius: 0.5, points: 4 }),
-            Shape2D(Circle { position: V3::X *  4, radius: 0.5, points: 5 }),
-            Shape2D(Circle { position: V3::X *  5, radius: 0.5, points: 6 }),
-            Shape2D(Circle { position: V3::X *  6, radius: 0.5, points: 7 }),
-            Shape2D(Circle { position: V3::X *  7, radius: 0.5, points: 8 }),
+    world.add("dog");
+    world.add("cat");
 
-            Shape3D(Cube   { position: V3::ZERO,     size:   V3::new(2.0, 0.1, 2.0)       }),
-            Shape3D(Sphere { position: V3::Y * 0.55, radius: 0.5, sectors: 32, stacks: 18 })
-        ])
-        .run();
+    let player_id = world.add("player");
+    let player    = world.get(player_id).expect("player == None");
+    println!("this should be player -> {}\n", player.name);
+
+    world.add("car");
+
+    world.debug();
 }
 
