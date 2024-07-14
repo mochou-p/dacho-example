aa66723147232d5e27ac40283af20db929aca22c
mochou-p <66534926+mochou-p@users.noreply.github.com>
2024-07-14 19:04:28 +0200

    Component removal

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,26 @@ use dacho::prelude::*;
 fn main() {
     let mut world = World::new();
 
-    let      e1_id = world.spawn_entity();
-    let    e1c1_id = world.spawn_child_entity(e1_id);
-    let _e1c1c1_id = world.spawn_child_entity(e1c1_id);
 
-    world.spawn_component(  e1_id, ComponentA);
+    let e1_id = world.spawn_entity();
+
+    world.spawn_component(e1_id, ComponentA);
+    world.spawn_component(e1_id, ComponentB);
+    world.spawn_component(e1_id, ComponentB);
+    world.spawn_component(e1_id, ComponentB);
+    world.spawn_component(e1_id, ComponentA);
+
+    world.remove_component::<ComponentA>(e1_id);  // removes the first found ComponentA
+    world.remove_components::<ComponentB>(e1_id); // removes all ComponentBs
+
+
+    let e1c1_id = world.spawn_child_entity(e1_id);
+
+    world.spawn_child_entity(e1c1_id);
     world.spawn_component(e1c1_id, ComponentB);
 
-    world.remove_entity(e1c1_id);
+    world.remove_entity(e1c1_id); // also removes the child and component above
+
 
     world.debug();
 }
