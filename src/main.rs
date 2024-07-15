878cf026c8188d517d4ec65d5e5237177b1f8df6
mochou-p <vrecusko20@gmail.com>
2024-07-15 09:38:01 +0200

    simplify callback data, remove redundant Id slice

--- src/main.rs
+++ src/main.rs
@@ -40,58 +40,50 @@ fn main() {
 
     world.spawn_components(player_id, 3, Chest);
 
-    world.call_mut(change_damage,   &[player_id], 100);
-    world.call_mut(uppercase_items, &[player_id], () );
+    // 2nd argument in World::call* is optional passtrough data,
+    // omittable with `()` for void, meaning nothing
+    world.call_mut(change_damage,   (player_id, 100));
+    world.call_mut(uppercase_items,  player_id      );
 
-    world.call(print_damage, &[player_id]);
-    world.call(print_items,  &[player_id]);
-    world.call(print_chests, &[player_id]);
+    world.call(print_damage, player_id);
+    world.call(print_items,  player_id);
+    world.call(print_chests, player_id);
 
 
 
     // world.debug();
 }
 
-fn change_damage(world: &mut World, ids: &[Id], new_damage: isize) {
-    if let Some(player_id) = ids.first() {
-        world.get_mut_component::<Weapon, _>(*player_id, |weapon| {
-            weapon.damage = new_damage;
-        });
+fn change_damage(world: &mut World, (player_id, new_damage): (Id, isize)) {
+    world.get_mut_component::<Weapon, _>(player_id, |weapon| {
+        weapon.damage = new_damage;
+    });
+}
+
+fn uppercase_items(world: &mut World, player_id: Id) {
+    world.get_mut_components::<Item, _>(player_id, |item| {
+        item.name = item.name.to_uppercase();
+    });
+}
+
+fn print_damage(world: &World, player_id: Id) {
+    if let Some(weapon) = world.get_component::<Weapon>(player_id) {
+        println!("player's damage = {}", weapon.damage);
     }
 }
 
-fn uppercase_items(world: &mut World, ids: &[Id], _: ()) {
-    if let Some(player_id) = ids.first() {
-        world.get_mut_components::<Item, _>(*player_id, |item| {
-            item.name = item.name.to_uppercase();
-        });
+fn print_items(world: &World, player_id: Id) {
+    print!("player's items = {{ ");
+
+    for item in &world.get_components::<Item>(player_id) {
+        print!("{}, ", item.name);
     }
+
+    println!("}}");
 }
 
-fn print_damage(world: &World, ids: &[Id]) {
-    if let Some(player_id) = ids.first() {
-        if let Some(weapon) = world.get_component::<Weapon>(*player_id) {
-            println!("player's damage = {}", weapon.damage);
-        }
-    }
-}
-
-fn print_items(world: &World, ids: &[Id]) {
-    if let Some(player_id) = ids.first() {
-        print!("player's items: ");
-
-        for item in &world.get_components::<Item>(*player_id) {
-            print!("{}, ", item.name);
-        }
-
-        println!();
-    }
-}
-
-fn print_chests(world: &World, ids: &[Id]) {
-    if let Some(player_id) = ids.first() {
-        println!("player has {} chests", world.get_components::<Chest>(*player_id).len());
-    }
+fn print_chests(world: &World, player_id: Id) {
+    println!("player has {} chests", world.get_components::<Chest>(player_id).len());
 }
 
 struct ComponentA;
