4dbe8e138b7803ee52da8c4bd28105d94039a060
mochou-p <vrecusko20@gmail.com>
2024-07-19 19:01:59 +0200

    bring the ECS to EventLoop, Application -> Game

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,21 @@
 use dacho::prelude::*;
 
 fn main() {
-    let mut world = World::new();
+    let mut game = Game::new("dacho example");
 
-    world.start(|world| {
-        let player_id = add_player(world);
-        capitalise_items(world, player_id);
-        print_items(world, player_id);
+    game.start(
+        |world| {
+            let player_id = add_player(world);
 
-        spawn_enemy(world, "bad guy");
-    });
+            capitalise_items(world, player_id);
+            print_items(world, player_id);
+            spawn_enemy(world, "bad guy");
+        }
+    );
 
-    world.update(|_| dots());
+    game.update(|_| print!("."));
 
-    world.run();
+    game.run();
 }
 
 struct Item {
@@ -45,24 +47,5 @@ fn spawn_enemy(world: &mut World, name: &str) {
     world.spawn_entity();
 
     println!("enemy \"{name}\" appeared");
-}
-
-fn dots() {
-    use std::io::{stdout, Write};
-
-    static mut I: usize = 0;
-
-    match unsafe { I } {
-        10_000_000 => {
-            unsafe { I = 0; }
-
-            print!(".");
-
-            stdout().flush().expect("failed to flush stdout");
-        },
-        _ => {
-            unsafe { I += 1; }
-        }
-    }
 }
 
