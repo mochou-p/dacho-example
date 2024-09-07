6c961cfc77f12c7cd481902364e68e208c43c3f5
mochou-p <66534926+mochou-p@users.noreply.github.com>
2024-09-08 00:56:13 +0200

    Query 1/all Entities, Entity get 1/all components

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,47 @@ use dacho::*;
 fn main() {
     let mut app = App::new("dacho example");
 
-    app.world.spawn((Player { name: "P1", level: 1 },));
-    app.world.add_system(level_up);
+    app.world.spawn((
+        Player { name: "Yonezu" },
+        Weapon { name: "Stick",      rarity: Rarity::Trash     },
+        Weapon { name: "Ashbringer", rarity: Rarity::Legendary }
+    ));
+
+    app.world.spawn((
+        Player { name: "Suda" },
+    ));
+
+    app.world.add_system(print_player_weapons);
     app.world.run();
 }
 
-fn level_up(query: &mut (Player,)) {
-    let (player,) = query;
+fn print_player_weapons(query: Query<(Player,)>) {
+    for entity in query.all() {
+        let Some(player) = entity.get_component::<Player>() else { continue; };
 
-    player.level += 1;
+        if let Some(weapons) = entity.get_components::<Weapon>() {
+            print!("Player `{}` has {} weapons {{ ", player.name, weapons.len());
 
-    println!("{} leveled up! ({})", player.name, player.level);
+            for weapon in &weapons {
+                print!("{} ({:?}), ", weapon.name, weapon.rarity);
+            }
+
+            println!("}}");
+        } else {
+            println!("Player `{}` has no weapons", player.name);
+        }
+    }
 }
 
 struct Player {
-    name:  &'static str,
-    level:          u8
+    name: &'static str
 }
+
+struct Weapon {
+    name:   &'static str,
+    rarity: Rarity
+}
+
+#[derive(Debug)]
+enum Rarity { Trash, _Common, _Uncommon, _Rare, _Epic, Legendary }
 
