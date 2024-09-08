e09f58303411d82a1c6455d47cc670c0a23efb70
mochou-p <66534926+mochou-p@users.noreply.github.com>
2024-09-08 11:40:20 +0200

    tuple-ify system arg to allow multiple queries

--- src/main.rs
+++ src/main.rs
@@ -8,34 +8,65 @@ fn main() {
     let mut app = App::new("dacho example");
 
     app.world.spawn((
-        Player { name: "Yonezu" },
-        Weapon { name: "Stick",      rarity: Rarity::Trash     },
-        Weapon { name: "Ashbringer", rarity: Rarity::Legendary }
+        Player       { name: "Yonezu" },
+        ActiveWeapon ( Weapon { name: "Stick", damage: 3.14, rarity: Rarity::Trash } ),
+        Weapon       { name: "Ashbringer", damage: 917.37, rarity: Rarity::Legendary }
     ));
 
     app.world.spawn((
         Player { name: "Suda" },
     ));
 
+    app.world.spawn((
+        Boss { health: 100.0 },
+    ));
+
     app.world.add_system(print_player_weapons);
+    app.world.add_system(damage_boss);
+
     app.world.run();
 }
 
-fn print_player_weapons(query: Query<(Player,)>) {
-    for entity in query.all() {
-        let Some(player) = entity.get_component::<Player>() else { continue; };
+fn print_player_weapons((player_query,): (Query<(Player,)>,)) {
+    if let Some(entities) = player_query.all() {
+        for entity in entities {
+            let Some(player) = entity.get_component::<Player>() else { continue; };
 
-        if let Some(weapons) = entity.get_components::<Weapon>() {
-            print!("Player `{}` has {} weapons {{ ", player.name, weapons.len());
+            if let Some(weapons) = entity.get_components::<Weapon>() {
+                print!("Player `{}` has {} weapons {{ ", player.name, weapons.len());
 
-            for weapon in &weapons {
-                print!("{} ({:?}), ", weapon.name, weapon.rarity);
+                if let Some(active_weapon) = entity.get_component::<ActiveWeapon>() {
+                    print!("\x1b[1m*{} ({:?})*\x1b[0m, ", active_weapon.0.name, active_weapon.0.rarity);
+                }
+
+                for weapon in &weapons {
+                    print!("{} ({:?}), ", weapon.name, weapon.rarity);
+                }
+
+                println!("}}");
+            } else {
+                println!("Player `{}` has no weapons", player.name);
             }
-
-            println!("}}");
-        } else {
-            println!("Player `{}` has no weapons", player.name);
         }
+    }
+}
+
+fn damage_boss((boss_query, player_query): (Query<(Boss,)>, Query<(Player, ActiveWeapon)>)) {
+    let mut damage = 0.0;
+
+    if let Some(entities) = player_query.all() {
+        for entity in entities {
+            let Some(weapon) = entity.get_component::<ActiveWeapon>() else { continue; };
+
+            damage += weapon.0.damage;
+        }
+    }
+
+    if damage > 0.0 {
+        let Some(entity) = boss_query.single()            else { return; };
+        let Some(boss)   = entity.get_component::<Boss>() else { return; };
+
+        println!("Boss health: {} -> {}", boss.health, boss.health - damage);
     }
 }
 
@@ -45,7 +76,14 @@ struct Player {
 
 struct Weapon {
     name:   &'static str,
+    damage: f32,
     rarity: Rarity
+}
+
+struct ActiveWeapon(Weapon);
+
+struct Boss {
+    health: f32
 }
 
 #[derive(Debug)]
