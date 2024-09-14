98ce7301fc88c0a5cf0d0b26c4295a5229f7191f
mochou-p <66534926+mochou-p@users.noreply.github.com>
2024-09-14 09:37:59 +0200

    Schedules: Start, Update

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,31 @@ use dacho::*;
 fn main() {
     let mut app = App::new("dacho example");
 
-    app.add_system(start);
+    app.add_system(Schedule::Start,  spawn_circle);
+    app.add_system(Schedule::Update, print_n);
 
     app.run();
 }
 
-fn start((query,): (Query<(WorldComponent,)>,)) {
+fn spawn_circle((query,): (Query<(WorldComponent,)>,)) {
     query.one().0.borrow().get(|mut world| {
         world.spawn((
             Mesh::circle(V3::ZERO, 0.5),
         ));
+
+        // temp
+        world.spawn((Always,));
     });
 }
+
+// forced component because QueryFn currently always expects QueryTuple as argument
+fn print_n(_: RunAlways) {
+    static mut N: u128 = 0;
+
+    println!("{}", unsafe { N += 1; N });
+}
+
+type RunAlways = (Query<(Always,)>,);
+
+struct Always;
 
