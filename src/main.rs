10b80e0372862956f0bc5549d9e4cdfafa4f4c0e
mochou-p <66534926+mochou-p@users.noreply.github.com>
2024-10-10 14:31:32 +0200

    Schedules: Keyboard, Mouse*

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,60 @@ use dacho::*;
 
 fn main() {
     App::new("dacho example")
-        .add_system(Schedule::Start,  spawn_circle)
-        .add_system(Schedule::Update, print_n)
+        .add_system(Schedule::Start,         spawn_circle)
+        .add_system(Schedule::Update,        print_dt)
+        .add_system(Schedule::Keyboard,      print_key)
+        .add_system(Schedule::MouseMovement, print_mouse_position)
+        .add_system(Schedule::MouseButton,   print_mouse_button)
+        .add_system(Schedule::MouseWheel,    print_mouse_wheel)
         .run();
 }
 
 fn spawn_circle((query,): (Query<(WorldComponent,)>,)) {
     query.one().0.borrow().get(|mut world| {
-        world.spawn((
-            Mesh::circle(V3::ZERO, 0.5),
-        ));
-
-        // temp
-        world.spawn((Always,));
+        world.spawn((MeshComponent::circle(V3::ZERO, 0.5),));
     });
 }
 
-// forced component because QueryFn currently always expects QueryTuple as argument
-fn print_n(_: RunAlways) {
-    static mut N: u128 = 0;
+fn print_dt((query,): (Query<(TimeComponent,)>,)) {
+    static mut N: usize = 0;
 
-    println!("{}", unsafe { N += 1; N });
+    let components = query.one();
+    let time       = components.0.borrow();
+
+    if unsafe { N } == 100 {
+        println!("{}", time.delta);
+        unsafe { N = 0; }
+    } else {
+        unsafe { N += 1; }
+    }
 }
 
-type RunAlways = (Query<(Always,)>,);
+fn print_key((query,): (Query<(KeyComponent,)>,)) {
+    let components = query.one();
+    let key        = components.0.borrow();
 
-struct Always;
+    println!("{:?}({})", key.code, key.down);
+}
+
+fn print_mouse_position((query,): (Query<(MousePositionComponent,)>,)) {
+    let components     = query.one();
+    let mouse_position = components.0.borrow();
+
+    println!("x: {}, y: {}", mouse_position.x, mouse_position.y);
+}
+
+fn print_mouse_button((query,): (Query<(MouseButtonComponent,)>,)) {
+    let components   = query.one();
+    let mouse_button = components.0.borrow();
+
+    println!("{:?}({})", mouse_button.button, mouse_button.down);
+}
+
+fn print_mouse_wheel((query,): (Query<(MouseWheelComponent,)>,)) {
+    let components   = query.one();
+    let mouse_scroll = components.0.borrow();
+
+    println!("x: {}, y: {}", mouse_scroll.x, mouse_scroll.y);
+}
 
