9a4458a4f426e3245256cefaeb01c2c379dcc631
mochou-p <66534926+mochou-p@users.noreply.github.com>
2025-02-02 23:23:23 +0100

    Camera

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,29 @@ use dacho::prelude::*;
 struct World {
     cursor_position: PhysicalPosition<f64>,
     window_size:     Vec3,
+    aspect_ratio:    Vec3,
+    movement:        Vec2,
+    clicked:         bool,
 
     commands: Commands,
-    meshes:   Meshes
+    meshes:   Meshes,
+    camera:   Camera
 }
 
 impl DachoWorld for World {
     #[inline]
-    #[must_use]
     fn get_mut_commands(&mut self) -> &mut Commands {
         &mut self.commands
     }
 
     #[inline]
-    #[must_use]
     fn get_mut_meshes(&mut self) -> &mut Meshes {
         &mut self.meshes
+    }
+
+    #[inline]
+    fn get_mut_camera(&mut self) -> &mut Camera {
+        &mut self.camera
     }
 }
 
@@ -32,6 +39,7 @@ fn main() {
     let game = Game::<World> {
         title:            "dacho example",
         start_systems:    &[start   ],
+        update_systems:   &[update  ],
         keyboard_systems: &[keyboard],
         mouse_systems:    &[mouse   ],
         cursor_systems:   &[cursor  ],
@@ -42,47 +50,104 @@ fn main() {
 }
 
 fn start(world: &mut World) {
-    world.window_size = Vec3 { x: 800.0, y: 600.0, z: 1.0 };
+    world.window_size  = Vec3 { x: 1600.0, y: 900.0, z: 1.0 };
 
-    world.meshes.push(Mesh::quad(Vec3::ZERO, Vec2::ONE * 0.1));
+    world.aspect_ratio = Vec3 {
+        x: world.window_size.x / world.window_size.y,
+        y: 1.0,
+        z: 1.0
+    };
+
+    world.meshes.push(
+        Mesh::quad(
+            Vec3::ZERO,
+            Vec2 { x: 1000.0 * world.aspect_ratio.x, y: 0.0003 }
+        )
+    );
+    world.meshes.push(
+        Mesh::quad(
+            Vec3::ZERO,
+            Vec2 { x: 0.0003, y: 1000.0 }
+        )
+    );
+}
+
+fn update(world: &mut World, time: &Time) {
+    if world.movement != Vec2::ZERO {
+        world.camera.move_by(
+            (world.movement.normalize() * time.delta)
+                .extend(0.0)
+        );
+
+        try_draw(world);
+    }
 }
 
 fn keyboard(world: &mut World, event: &KeyEvent) {
-    if event.repeat || !event.state.is_pressed() {
+    use {
+        PhysicalKey::Code,
+        KeyCode::{
+            ArrowDown, ArrowLeft, ArrowRight, ArrowUp, Escape,
+            KeyA, KeyD, KeyQ, KeyS, KeyW, Space
+        }
+    };
+
+    if event.repeat {
         return;
     }
 
-    if event.physical_key == PhysicalKey::Code(KeyCode::Escape) {
-        world.commands.submit(Command::Exit);
+    let s = helpers::sign_from_element_state(event.state);
+
+    match event.physical_key {
+        Code(Escape | KeyQ) => {
+            world.commands.submit(Command::Exit);
+        },
+
+        Code(KeyW | ArrowUp   ) => { world.movement.y -= 1.0 * s; },
+        Code(KeyA | ArrowLeft ) => { world.movement.x -= 1.0 * s; },
+        Code(KeyS | ArrowDown ) => { world.movement.y += 1.0 * s; },
+        Code(KeyD | ArrowRight) => { world.movement.x += 1.0 * s; },
+
+        Code(Space) => if event.state.is_pressed() {
+            world.camera.move_to(Vec2::ZERO);
+        },
+        _ => ()
     }
 }
 
 fn mouse(world: &mut World, button: MouseButton, state: ElementState) {
-    if !state.is_pressed() {
-        return;
-    }
-
     if matches!(button, MouseButton::Left) {
+        world.clicked = state.is_pressed();
+    }
+}
+
+fn cursor(world: &mut World, position: &PhysicalPosition<f64>) {
+    world.cursor_position = *position;
+
+    try_draw(world);
+}
+
+fn try_draw(world: &mut World) {
+    if world.clicked {
+        let mut pos  = helpers::cursor_position_to_vec3(&world.cursor_position);
+        pos         /= world.window_size;
+        pos         -= consts::VEC3_HALF_XY;
+        pos         *= world.aspect_ratio;
+        pos         += world.camera.position.truncate().extend(0.0);
+
         world.meshes.push(
             Mesh::quad(
-                helpers::cursor_position_to_vec3(&world.cursor_position)
-                    / world.window_size
-                    - consts::VEC3_HALF,
-                Vec2::splat(0.2)
+                pos,
+                Vec2::splat(0.0075)
             )
         );
     }
 }
 
-#[inline]
-fn cursor(world: &mut World, position: &PhysicalPosition<f64>) {
-    world.cursor_position = *position;
-}
-
 mod consts {
     use dacho::prelude::*;
 
-    pub static VEC3_HALF: Vec3 = Vec3::splat(0.5);
+    pub static VEC3_HALF_XY: Vec3 = Vec3 { x: 0.5, y: 0.5, z: 0.0 };
 }
 
 mod helpers {
@@ -93,6 +158,12 @@ mod helpers {
     #[expect(clippy::cast_possible_truncation, reason = "no need for double precision")]
     pub const fn cursor_position_to_vec3(position: &PhysicalPosition<f64>) -> Vec3 {
         Vec3 { x: position.x as f32, y: position.y as f32, z: 0.0 }
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn sign_from_element_state(state: ElementState) -> f32 {
+        f32::from(i8::from(state.is_pressed()) * 2 - 1)
     }
 }
 
